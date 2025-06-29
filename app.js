c0182417b6a2af14e8ab4bb733861a45f7fa7bae
Eliemenye Kester <kestereliemenye@gmail.com>
2025-06-29 15:37:54 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+
+  const links = document.querySelectorAll('.nav-link');
+  const currentPage = window.location.pathname.split('/').pop();
+
+  links.forEach(link => {
+    if (link.getAttribute('href') === currentPage) {
+      link.classList.add('active');
+    }
+  });
+
